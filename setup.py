a936615370d8b1a2ce0ea8b4ae1569837729ccf5
James Jahraus <james.jahraus@gmail.com>
2024-02-08 18:31:01 -0700

    Added Jennifer's Library  __init__

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(
+    name='gis3011py',
+    version='0.1.0',
+    description='Reusable functions for GIS 3011',
+    author='Jennifer Muha',
+    install_requires=['psycopg2'],
+    packages=['gis3011py']
+)
